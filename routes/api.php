18bbc8345294166bcabe6b759f6ac1b5d128dd97
heuder.sena <Heuder.Sena@sicoob.com.br>
2026-05-06 23:43:52 -0300

    [main]: Add authentication and profile management features
    - Implemented user registration, login, and password reset functionality.
    - Created views for user authentication including login, registration, and password reset.
    - Added email verification support for new users.
    - Developed profile management features allowing users to update their information and delete their accounts.
    - Introduced reusable Blade components for buttons, inputs, and navigation links.
    - Enhanced UI with Tailwind CSS for a responsive design.
    - Added tests for authentication, email verification, and profile management to ensure functionality.

--- routes/api.php
+++ routes/api.php
@@ -7,10 +7,6 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/login', function (Request $request) {
-    return "login";
-})->name('login');
-
 Route::middleware(['tenant'])->group(function () {
 
     Route::get('/teste', function (Request $request) {
